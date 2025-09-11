d0b158a5149c3ecdfdff39b0223822b039bf4244
Blaise2004 <blaisetreverton@icloud.com>
2025-09-10 20:05:18 -0400

    Added some buildings
    Added buildings 1-4 so far

--- topics/instructions/instructions challenge/template-p5-project/js/InstructionChallenge.js
+++ topics/instructions/instructions challenge/template-p5-project/js/InstructionChallenge.js
@@ -13,7 +13,7 @@
 */
 function setup() {
     createCanvas(800, 600);
-    background("#eeecb0ff");
+    background("#b0deeeff");
 }
 
 
@@ -24,7 +24,32 @@ function draw() {
 
     push();
     stroke(2);
-    fill("#18b2d9dd");
-    rect(x, y, w, h, [tl], [tr], [br], [bl])
+    fill("#1a7027dd");
+    rect(0, 400, 800, 300);
+    pop();
 
+    skyScaper1();
+    skyScaper2();
+    skyScaper3();
+    skyScaper4();
+}
+
+function skyScaper1() { 
+    fill("#585858ff");
+    rect(100, 200, 80, 200);
+}
+
+function skyScaper2() { 
+    fill("#d17f20ff");
+    rect(200, 100, 80, 300);
+}
+
+function skyScaper3() { 
+    fill("#4b32c4ff");
+    triangle(300, 400, 400 , 400, 350, 100);
+}
+
+function skyScaper4() { 
+    fill("#b3c432ff");
+    ellipse(500, 400, 100);
 }
