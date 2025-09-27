e58ef063820f700c2b93f3ef71537203ca5e7373
Blaise2004 <blaisetreverton@icloud.com>
2025-09-27 16:58:18 -0400

    making moon colours and arc

--- topics/ArtJam/template-p5-project/js/script.js
+++ topics/ArtJam/template-p5-project/js/script.js
@@ -11,19 +11,59 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
+let colourclouds = {
+    leftside: "#faf3e3",
+    rightside: '#ebcaaa'
+}
+let colourmoon = {
+    leftside: "#faf3e3",
+    rightside: '#ebcaaa'
+}
+
 function setup() {
     createCanvas(800, 600,);
     background(220); 
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+
+
+
 function draw() {
-    background(220);
-    fill(255, 0, 0);
-    ellipse(mouseX, mouseY, 50, 50);
+    noStroke();
+drawSky();
+drawMoon();
+drawClouds();
+drawMountains();
+drawHills();
+drawTrees();
+drawFirepit();
+drawTent();
 }
 
+function drawSky() {
+    let skycolour1 = '#65687bff';
+    background (skycolour1)
+}
+function drawMoon() {
+  
 
+    fill(colourmoon.rightside);
+    ellipse(200, 100, 50);
+
+    fill(colourmoon.leftside)
+   arc(196, 100, 40, 50, 1.9,11.5); // top half overlay
+
+}
+function drawClouds() {
+    push();
+    fill("#faf3e3")
+    ellipse();
+        pop();
+}
+
+function drawMountains() {}
+function drawHills() {}
+function drawTrees() {}
+function drawFirepit() {}
+function drawTent() {}
